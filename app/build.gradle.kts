df248f6e0d5b7ce508b0ef8b8737fbbe689363eb
Robert Lisowski <46828559+TheUnsleepingAlchemist@users.noreply.github.com>
2026-09-12 07:38:50 +0200

    feat: add additional libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -51,4 +52,10 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+
+    implementation(libs.bundles.lifecycle)
+    implementation(libs.bundles.nav3)
+    implementation(libs.bundles.ktor)
+    implementation(libs.bundles.coil)
+
 }
