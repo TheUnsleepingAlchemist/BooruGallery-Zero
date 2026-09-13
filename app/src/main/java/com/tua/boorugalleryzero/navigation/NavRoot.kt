259cb23a980e4a297c25efc8d0bdb5a75715382f
Robert Lisowski <46828559+TheUnsleepingAlchemist@users.noreply.github.com>
2026-09-13 10:38:49 +0200

    feat: add gif support

--- app/src/main/java/com/tua/boorugalleryzero/navigation/NavRoot.kt
+++ app/src/main/java/com/tua/boorugalleryzero/navigation/NavRoot.kt
@@ -1,16 +1,31 @@
 package com.tua.boorugalleryzero.navigation
 
+import android.os.Build.VERSION.SDK_INT
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.runtime.rememberNavBackStack
 import androidx.navigation3.ui.NavDisplay
+import coil3.ImageLoader
+import coil3.gif.AnimatedImageDecoder
+import coil3.gif.GifDecoder
 import com.tua.boorugalleryzero.presentation.screens.home.HomeScreen
 
 @Composable
 fun NavRoot(modifier: Modifier = Modifier) {
 
     val backStack = rememberNavBackStack(RouteRoot.HomeScreen)
+
+    val gifLoader = ImageLoader.Builder(LocalContext.current.applicationContext)
+        .components {
+            if (SDK_INT >= 28) {
+                add(AnimatedImageDecoder.Factory())
+            } else {
+                add(GifDecoder.Factory())
+            }
+        }
+        .build()
 
     NavDisplay(
         backStack = backStack,
