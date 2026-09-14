0d882ec59cab49638524cce434c8d56048f8cf06
Robert Lisowski <46828559+TheUnsleepingAlchemist@users.noreply.github.com>
2026-09-14 09:27:27 +0200

    chore: add fixed font size range in unsuported file formats

--- app/src/main/java/com/tua/boorugalleryzero/presentation/components/PreviewUnknown.kt
+++ app/src/main/java/com/tua/boorugalleryzero/presentation/components/PreviewUnknown.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 import com.tua.boorugalleryzero.R
 
@@ -34,9 +35,9 @@ fun PreviewUnknown(
             minLines = 3,
             maxLines = 3,
             color = Color.Black,
-            autoSize = TextAutoSize.StepBased(),
+            autoSize = TextAutoSize.StepBased(minFontSize = 5.sp, maxFontSize = 20.sp),
             textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth(0.6f).align(BiasAlignment(0.9f,-0.75f))
+            modifier = Modifier.fillMaxWidth(0.7f).align(BiasAlignment(0.9f,-0.75f))
         )
     }
 }
